51a2ead6af3b85919db924d8082f41fe5dcfab87
Stanislav Odnoletko <s.odnoletko@aristos.pw>
2021-08-14 18:42:22 +0300

    1.1.2 Изменён шаблон под версию Vue 3.2

--- generator/templateV3/src/components/TempComponent/TempComponent.vue
+++ generator/templateV3/src/components/TempComponent/TempComponent.vue
@@ -1,13 +1,8 @@
 <template>
   <div></div>
 </template>
-<script>
-import { defineComponent } from 'vue';
-import <%= options.componentName %> from './<%= options.componentName %>.module.js'
-export default defineComponent({
- name:  '<%= options.componentName %>',
- setup(props, context){}
-})
+<script setup>
+import <%= options.componentName %> from './<%= options.componentName %>.module.js';
 </script>
 <style lang="scss">
 @import "<%= options.componentName %>.style";
